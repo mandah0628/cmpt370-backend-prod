9617c0a6c3b6d7e3a812284c65d033ae2661a7ac
Matt Tsogt <mandah0628@gmail.com>
2025-04-08 19:00:46 -0600

    fixed param

--- src/controller/listingController.js
+++ src/controller/listingController.js
@@ -231,7 +231,7 @@ const deleteListing = async (req, res) => {
 // get listing data by its id
 const getListingById = async (req, res) => {
     try {
-        const listingId = req.params.listingId;
+        const listingId = req.params.id;
         
         if (!listingId) {
             return res.status(400).json({ message: "Missing listing ID" });
